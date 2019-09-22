02aa46f44f8aed52b14e1b8c110390dc7a2ef91e
electrone901 <carbajalluis@rocketmail.com>
2019-09-22 12:16:02 -0400

    added result of weight view

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -5,18 +5,50 @@ import current from '../img/current.png';
 // import useWindowSize from 'react-use/lib/useWindowSize'
 import Confetti from 'react-confetti'
 
+const config = {
+    angle: 90,
+    spread: 45,
+    startVelocity: 45,
+    elementCount: 50,
+    dragFriction: 0.1,
+    duration: "100",
+    stagger: 0,
+    width: "10px",
+    height: "10px",
+    colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
+  };
+
 class Dashboard extends Component {
     constructor() {
         super();
         this.state = {
             previewPost: false,
             current: true,
+            time: false,
+            currentCount: 3,
+            numberC: 100
         }
     }
 
     componentDidMount() {
-        window.scrollTo(0,0);        
+        window.scrollTo(0,0);  
+        this.intervalId = setInterval(this.timer.bind(this), 1000);
+        console.log(this.state.currentCount)
+        // useEffect();      
     }
+    timer() {
+        this.setState({
+          currentCount: this.state.currentCount - 1
+        })
+        console.log(this.state.currentCount)
+        if(this.state.currentCount < 1) { 
+            console.log("stop")
+            this.setState({
+                numberC: 0
+              })
+        }
+      }
+
 
     getFavoritesDeals() {
         // toggle userDealsclass && favoriteclass to active
@@ -34,10 +66,24 @@ class Dashboard extends Component {
         }
     }
 
+    // useEffect() {
+    //     setCounter = 0;
+    //     const interval = setInterval(() => {
+    //         setCounter(0);
+    //     }, 2000);
+
+    //     return () => {
+    //         clearInterval(interval);
+    //         this.setState({
+    //             time:false
+    //         })
+    //     }
+    // }
+
     render() {
 
         // const { width, height } = useWindowSize();
-
+        
         let preview = <div className="container">
             <br/>
             <img src={history} className="figure-img img-fluid moreHeight" alt="logo"/> 
@@ -51,6 +97,7 @@ class Dashboard extends Component {
         return(
             <div className="container text-center">
                 <Confetti
+                numberOfPieces={this.state.numberC}
                     // width={width}
                     // height={height}
                 />
